42db64d3c10aa4c7b666d854d80b7843afdcddfe
Lester Ivan <lesterivan.dalaguit@gmail.com>
2022-09-09 17:07:35 +0800

    map through state array

--- src/App.js
+++ src/App.js
@@ -1,69 +1,63 @@
 import './index.css';
 import Employee from './Components/employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState();
+  const [employees, setEmployees] = useState([
+      {
+        name: "Lester Ivan Dalaguit",
+        role: "Software Engineer",
+        img: "https://images.pexels.com/photos/2102413/pexels-photo-2102413.jpeg?"
+      },
+      {
+        name: "Sarah Lahbati",
+        role: "Product Manager",
+        img: "https://images.pexels.com/photos/1181672/pexels-photo-1181672.jpeg?"
+      },
+      {
+        name: "John Doe",
+        role: "Designer",
+        img: "https://images.pexels.com/photos/3201580/pexels-photo-3201580.jpeg?"
+      },
+      {
+        name: "Lander Ace Dalaguit",
+        role: "Graphic Designer",
+        img: "https://images.pexels.com/photos/1181676/pexels-photo-1181676.jpeg?"
+      },
+      {
+        name: "Enting Kabesote",
+        role: "Marketing Manager",
+        img: "https://images.pexels.com/photos/7110/desk-office-workspace-coworking.jpg?"
+      },
+      {
+        name: "Kardo Dalisay",
+        role: "The Immortal",
+        img: "https://images.pexels.com/photos/6972/summer-office-student-work.jpg?"
+      },
+    ]);
+
   const showEmployees = true;
   return (
       <div className="App">
         {showEmployees ? (
           <>
-            <input type='text' onChange={(e) => {
+            {/* <input type='text' onChange={(e) => {
               setRole(e.target.value)
             }}
-            />
+            /> */}
             <div className='flex flex-wrap justify-center'>
-              <Employee
-                name="Lester Ivan Dalaguit"
-                role="Software Engineer"
-                img="https://images.pexels.com/photos/2102413/pexels-photo-2102413.jpeg?cs=srgb&dl=pexels-djordje-petrovic-2102413.jpg&fm=jpg"
-              />
-              <Employee
-                name="Sarah Labati"
-                role="Product Manager"
-                img="https://images.pexels.com/photos/1181672/pexels-photo-1181672.jpeg?cs=srgb&dl=pexels-christina-morillo-1181672.jpg&fm=jpg"
-              />
-              <Employee
-                name="John Doe"
-                role="Intern"
-                img="https://images.pexels.com/photos/3201580/pexels-photo-3201580.jpeg?cs=srgb&dl=pexels-cottonbro-3201580.jpg&fm=jpg"
-              />
-              <Employee
-                name="Lester Ivan Dalaguit"
-                role="Software Engineer"
-                img="https://images.pexels.com/photos/2102413/pexels-photo-2102413.jpeg?cs=srgb&dl=pexels-djordje-petrovic-2102413.jpg&fm=jpg"
-              />
-              <Employee
-                name="Sarah Labati"
-                role="Product Manager"
-                img="https://images.pexels.com/photos/1181672/pexels-photo-1181672.jpeg?cs=srgb&dl=pexels-christina-morillo-1181672.jpg&fm=jpg"
-              />
-              <Employee
-                name="John Doe"
-                role="Intern"
-                img="https://images.pexels.com/photos/3201580/pexels-photo-3201580.jpeg?cs=srgb&dl=pexels-cottonbro-3201580.jpg&fm=jpg"
-              />
-              <Employee
-                name="Lester Ivan Dalaguit"
-                role="Software Engineer"
-                img="https://images.pexels.com/photos/2102413/pexels-photo-2102413.jpeg?cs=srgb&dl=pexels-djordje-petrovic-2102413.jpg&fm=jpg"
-              />
-              <Employee
-                name="Sarah Labati"
-                role="Product Manager"
-                img="https://images.pexels.com/photos/1181672/pexels-photo-1181672.jpeg?cs=srgb&dl=pexels-christina-morillo-1181672.jpg&fm=jpg"
-              />
-              <Employee
-                name="John Doe"
-                role="Intern"
-                img="https://images.pexels.com/photos/3201580/pexels-photo-3201580.jpeg?cs=srgb&dl=pexels-cottonbro-3201580.jpg&fm=jpg"
-              />
-              <Employee
-                name="Lester Ivan Dalaguit"
-                role="Software Engineer"
-                img="https://images.pexels.com/photos/2102413/pexels-photo-2102413.jpeg?cs=srgb&dl=pexels-djordje-petrovic-2102413.jpg&fm=jpg"
-              />
+              {employees.map((employee)=> {
+                return (
+                  <Employee
+                    key={uuidv4()}
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                  />
+                )
+              })}
             </div>
           </>
         ) : (
